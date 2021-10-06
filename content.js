ec8c2c10d4a2dc2335cc690e20cdd6a1267ac7a6
Triceratroll <adrianmf06@gmail.com>
2021-10-07 00:52:55 +0200

    add icons languages fixed url & css

--- content.js
+++ content.js
@@ -1,16 +1,16 @@
 chrome.runtime.sendMessage({todo: "showPageAction"});
-//console.log("Content!")
+console.log("Content!")
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.todo === "fillUser"){
         var user = request.storedUser;
         document.getElementsByName("dni")[0].value = user;
-        //console.log(user);
+        console.log(user);
     }
     if(request.todo === "fillPass"){
         var pass = request.storedPass;
         document.getElementsByName("clau")[0].value = pass;
-        //console.log(pass);
+        console.log(pass);
     }
     document.getElementsByClassName('upv_btsubmit')[0].click();
 });
